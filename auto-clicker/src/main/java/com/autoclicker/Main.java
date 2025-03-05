0e72672827728746562f5c6c4614b9a34c618c4e
Jerome Hillock <jeromeydawinner@gmail.com>
2025-03-05 01:12:08 -0500

    fixed maven project

---
+++ auto-clicker/src/main/java/com/autoclicker/Main.java
@@ -0,0 +1,31 @@
+package com.autoclicker;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        // new JFrame with the title Auto Clicker
+        JFrame frame = new JFrame("Auto Clicker");
+        // JFrames hold JPanels, JLabels, JButtons, etc
+        frame.getContentPane();
+
+        // creates App object app
+        App app = new App();
+
+        Dimension size = new Dimension(900, 900);
+        app.setPreferredSize(size);
+        app.setMinimumSize(size);
+        app.setMaximumSize(size);
+
+        // adds app object a frame
+        frame.add(app);
+
+        // frame set upaaaaaaaaa
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // adding default settings
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
